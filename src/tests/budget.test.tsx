006fbcf7d81621b013b19356d8fa0c79e1365de9
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-17 01:48:20 +0530

    Update the budget testing

--- src/tests/budget.test.tsx
+++ src/tests/budget.test.tsx
@@ -61,4 +61,161 @@ describe("should test the budget form",()=>{
             );
           });
         });
+        test("handles invalid login with alert for no user found", async () => {
+            (axios.post as jest.Mock).mockResolvedValue({
+              status: 404,
+            });
+        
+            window.alert = jest.fn();
+        
+            render(
+              <dataContext.Provider
+                value={{
+                  username:"Vinaysai"
+                }}
+              >
+                <Budget/>
+              </dataContext.Provider>
+            );
+        
+            fireEvent.change(screen.getByTestId("titleInput"), {
+              target: { value: "Vinay" },
+            });
+            fireEvent.change(screen.getByTestId("amountInput"), {
+              target: { value: "5000" },
+            });
+        
+            fireEvent.click(screen.getByTestId("submitButton"));
+        
+            await waitFor(() => {
+              expect(axios.post).toHaveBeenCalledWith(
+                "http://localhost:5005/api/budget/Vinaysai",
+                {
+                    category: "Vinay",
+                    amount: "5000",
+                }
+              );
+              expect(window.alert).toHaveBeenCalledWith(
+                "No budget found please create the budget"
+              );
+              
+            });
+          });
+          test("handles invalid login with alert for no user found", async () => {
+            (axios.post as jest.Mock).mockResolvedValue({
+              status: 200,
+            });
+        
+            window.alert = jest.fn();
+        
+            render(
+              <dataContext.Provider
+                value={{
+                  username:"Vinaysai"
+                }}
+              >
+                <Budget/>
+              </dataContext.Provider>
+            );
+        
+            fireEvent.change(screen.getByTestId("titleInput"), {
+              target: { value: "Vinay" },
+            });
+            fireEvent.change(screen.getByTestId("amountInput"), {
+              target: { value: "5000" },
+            });
+        
+            fireEvent.click(screen.getByTestId("submitButton"));
+            await waitFor(() => {
+              expect(axios.post).toHaveBeenCalledWith(
+                "http://localhost:5005/api/budget/Vinaysai",
+                {
+                    category: "Vinay",
+                    amount: "5000",
+                }
+              );
+              expect(window.alert).toHaveBeenCalledWith(
+                "Budget created succesfully"
+              );
+              
+            });
+          });
+
+
+
+          test("handles error in while creating new budget", async () => {
+            (axios.post as jest.Mock).mockRejectedValue(new Error("Failed request"));
+            const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+            render(
+              <dataContext.Provider
+                value={{
+                  username:"Vinaysai"
+                }}
+              >
+                <Budget/>
+              </dataContext.Provider>
+            );
+        
+            fireEvent.change(screen.getByTestId("titleInput"), {
+              target: { value: "Vinay" },
+            });
+            fireEvent.change(screen.getByTestId("amountInput"), {
+              target: { value: "5000" },
+            });
+        
+            fireEvent.click(screen.getByTestId("submitButton"));
+        
+            await waitFor(() => {
+              expect(axios.post).toHaveBeenCalledWith(
+                "http://localhost:5005/api/budget/Vinaysai",
+                {
+                    category: "Vinay",
+                    amount: "5000",
+                }
+              );
+              expect(console.error).toHaveBeenCalledWith("",
+                expect.anything() );
+              
+            });
+          });
+          
+          test("handles invalid login with alert for no user found", async () => {
+            (axios.post as jest.Mock).mockResolvedValue({
+              status: 200,
+            });
+        
+            window.alert = jest.fn();
+        
+            render(
+              <dataContext.Provider
+                value={{
+                  username:"Vinaysai"
+                }}
+              >
+                <Budget/>
+              </dataContext.Provider>
+            );
+        
+            fireEvent.change(screen.getByTestId("titleInput"), {
+              target: { value: "Vinay" },
+            });
+            fireEvent.change(screen.getByTestId("amountInput"), {
+              target: { value: "5000" },
+            });
+        
+            fireEvent.click(screen.getByTestId("submitButton"));
+            await waitFor(() => {
+              expect(axios.post).toHaveBeenCalledWith(
+                "http://localhost:5005/api/budget/Vinaysai",
+                {
+                    category: "Vinay",
+                    amount: "5000",
+                }
+              );
+              expect(window.alert).toHaveBeenCalledWith(
+                "Budget created succesfully"
+              );
+              
+            });
+          });
     })
